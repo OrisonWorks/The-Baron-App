f8036304aefa23c4ea80f4e569f584814d7df40e
tumelomwaba <mwabatd@napsa.co.zm>
2026-06-12 15:20:28 +0200

    WIP: Products

--- frontend/frontend-app/src/ToastContext.js
+++ frontend/frontend-app/src/ToastContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback, useContext, useEffect, useMemo, useState } from 'react';
+import React, { createContext, useCallback, useContext, useMemo, useState } from 'react';
 import './Toast.css';
 
 const ToastContext = createContext({ showToast: (_msg) => {} });
